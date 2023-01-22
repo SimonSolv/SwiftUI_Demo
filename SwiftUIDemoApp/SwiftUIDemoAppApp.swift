3f7a89a1f85b77e5b2547f3422d1e35ff4f03206
SimonSolv <4to5work@gmail.com>
2023-01-22 14:24:34 +0300

    Initial Commit

---
+++ SwiftUIDemoApp/SwiftUIDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoAppApp.swift
+//  SwiftUIDemoApp
+//
+//  Created by Simon Pegg on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
